50bab09edb0416073977b9a7b09d9199440c72c0
davpoolechem <davpoole@iastate.edu>
2019-04-09 14:08:00 -0500

    Add basic typesetting to do loop

--- examples/for_loop_transcompile.jl
+++ examples/for_loop_transcompile.jl
@@ -1,5 +1,7 @@
 function julia90(filename_jl)
-    #copy python file to julia array object for manipulation
+    #**********************#
+    #* read in julia file *#
+    #**********************#
     f_jl::IOStream = open(filename_jl)
         file::Array{String,1} = readlines(f_jl)
     close(f_jl)
@@ -7,8 +9,10 @@ function julia90(filename_jl)
     file_start = 1
     file_end = 1
 
+    #****************************************#
+    #* extract function to be transcompiled *#
+    #****************************************#
     for i in 1:length(file)
-        println(length(file))
         if (occursin("function hello_julia_impl",file[i]))
             file_start = i
         end
@@ -17,7 +21,21 @@ function julia90(filename_jl)
         end
     end
 
+    #***********************#
+    #* handle type setting *#
+    #***********************#
+    for i in 1:length(file[file_start:file_end])
+        if (occursin(r"[ *](.*)\:\:Int64",file[i]))
+            variable = match(r"[ *](.*)\:\:Int64",file[i])[1]
+            println(variable)
+            file[i] = replace(file[i],file[i]=>"    INTEGER :: $variable ")
+        end
+    end
+
+    #******************#
+    #* handle do loop *#
+    #******************#
     display(file[file_start:file_end])
 end
 
-test("examples/hello_julia.jl")
+julia90("examples/hello_julia.jl")
